18785188a4da8cb5917fb2de220bcf8116714576
amir-saeed <devloggeruk@gmail.com>
2017-03-30 01:47:18 +0100

    added validation

--- Show-bootstrap-modal-on-button-click/src/js/components/Form.js
+++ Show-bootstrap-modal-on-button-click/src/js/components/Form.js
@@ -5,26 +5,36 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('form is valid');
+    }
+
     render() {
         return (
             <div className="form-section">
                 <div className="container">
                     <div className="row">
                         <div class="col-md-5 col-md-offset-3">
-                            <form id="login-form" role="form" action="#">
+                            <form id="login-form" role="form" onSubmit={this.handleSubmit}>
                                 <div class="form-group">
                                     <label for="login-email">Email</label>
                                     <input id="login-email"
                                         class="email form-control input-md"
                                         type="email"
-                                        placeholder="Email" />                                    
+                                        placeholder="Email"
+                                        required={true}
+                                    />
                                 </div>
                                 <div class="form-group">
                                     <label for="login-password">Password</label>
                                     <input id="login-password"
                                         class="form-control input-md"
                                         type="password"
-                                        placeholder="Password" />
+                                        placeholder="Password" 
+                                        required={true}
+                                        />
                                 </div>
                                 <div class="form-group">
                                     <div className="pull-left">
